5e053ca28f160ade3e67fdc7469535dc3d5786d8
Chen Yang <yangcnju@gmail.com>
2020-06-27 16:52:19 -0400

    color code airline names

--- src/components/AirlineItem.vue
+++ src/components/AirlineItem.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="d-flex card card-body m-1 p-1 border border-primary">
     <div class="d-flex flex-row">
+      <div v-if="['国航', '南航', '厦航', '东航', '山东航空', '青岛航空'].includes(name)" class="card-text text-secondary">
+        <strong>{{name}}</strong>
+      </div>
+      <div v-else class="card-text text-primary">
+        <strong>{{name}}</strong>
+      </div>
       <a :href="searchLink" target="_blank"
         @click="incrementCount"
       >
-        <span class="card-text">{{name}} </span>
         <span class="card-text">{{flight}} </span>
         <span class="card-text">{{origCode}} - {{destCode}}</span>
         <span class="card-text"> {{date}}</span>
